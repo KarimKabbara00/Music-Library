c8e4e3a4529e97eee17b074857a7033ddc501a9d
KarimKabbara00 <karim.kabbara00@gmail.com>
2023-11-04 10:27:31 -0600

    Created Project

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# window configuration
+tk = Tk()
+tk.overrideredirect()
+width = tk.winfo_screenwidth()
+height = tk.winfo_screenheight()
+tk.geometry("%dx%d" % (width - 10, height - 10))
+tk.state('zoomed')
+tk.winfo_toplevel().title('Music Library')
+
+# launch application
+tk.mainloop()
