0b15482ea027e9e89b814bd2f65ae34b8fa18856
KarimKabbara00 <karim.kabbara00@gmail.com>
2023-11-04 10:35:57 -0600

    Created Project

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ height = tk.winfo_screenheight()
 tk.geometry("%dx%d" % (width - 10, height - 10))
 tk.state('zoomed')
 tk.winfo_toplevel().title('Music Library')
-print()
+
 # launch application
 tk.mainloop()
